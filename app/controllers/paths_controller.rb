5b564526edc242dafe38a38b2fc3b86f898ef319
kimberley <yoyijun@gmail.com>
2016-09-28 20:34:27 +0800

    [ Path CRUD ] add paths#new

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -2,4 +2,20 @@ class PathsController < ApplicationController
   def index
     @paths = Path.all
   end
+
+  def new
+  end
+
+  def create
+    @path = Path.new(path_params)
+
+    @path.save
+    redirect_to action: :index
+  end
+
+  private
+
+  def path_params
+    params.require(:path).permit(:title, :url)
+  end
 end
