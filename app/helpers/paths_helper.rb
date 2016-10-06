fc0378e38ce9cbc7dda8858dfcd4aadf2d232270
Kimberley <kimberley.yao@tarokosoftware.com>
2016-10-06 10:58:25 +0800

    [Mission CRUD] DRY the queries by using scope

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -1,7 +1,7 @@
 module PathsHelper
   def if_finish_all_missions?(course)
     all_missions = course.missions.count
-    finished_missions = course.missions.where(if_finish: true).count
+    finished_missions = course.missions.finished.count
 
     result = (all_missions == finished_missions)
   end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,6 +1,8 @@
 class Mission < ApplicationRecord
   belongs_to :course
 
+  scope :finished, -> { where(if_finish: true) }
+
   def to_param
     title
   end
