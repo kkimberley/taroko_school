ffe10ef80bb0d792f820ea42d45fff5b2edeb824
Kimberley <kimberley.yao@tarokosoftware.com>
2016-10-13 17:49:38 +0800

    [Post CRUD] add create, read, update, delete and show for Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005030503) do
+ActiveRecord::Schema.define(version: 20161013092723) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20161005030503) do
   create_table "paths", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.text     "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
